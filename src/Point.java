3231c29cf6020f9cc0b2c35f29946de55307817f
Zhao Zhang <zhaoz@nycstudents.net>
2023-10-02 09:14:19 -0400

    commit

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+    private int x;
+    private int y;
+    public Point(int setX, int setY){
+        x = setX;
+        y = setY;
+    }
+    public Point(int set){
+        x = set;
+        y = set;
+    }
+    public Point(){
+        x = 0;
+        y = 0;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public void setX(int newPointX){
+        x = newPointX;
+    }
+    public void setY(int newPointY){
+        y = newPointY;
+    }
+    public String coordinate(){
+        return "("+x+", "+y+")";
+    }
+    public String quadrant(){
+        if (x > 0 && y > 0){
+            return "I";
+        } else if (x < 0 && y > 0) {
+            return "II";
+        } else if (x < 0 && y < 0) {
+            return "III";
+        } else if (x > 0 && y < 0) {
+            return "IV";
+        } else if (x == 0 && y == 0) {
+            return "origin";
+        } else {
+            return "on an axis";
+        }
+    }
 }
--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class RectangleRunner {
+public class RectangleRunner {
+    public static void main(String [] args){
+        Rectangle plot1 = new Rectangle(150, 200);
+        plot1.setWidth(75);
+        Rectangle plot2 = new Rectangle(125);
+        plot2.setWidth(75);
+        plot2.setLength(75);
+        Rectangle plot3 = new Rectangle();
+        plot3.setWidth(75);
+        int totalArea = (24375-plot2.calculateArea()-plot3.calculateArea())/75;
+        plot1.setLength(totalArea);
+        System.out.println("The area of plot 1 is: "+plot1.calculateArea());
+        System.out.println("The area of plot 2 is: "+plot2.calculateArea());
+        System.out.println("The area of plot 3 is: "+plot3.calculateArea());
+    }
 }
